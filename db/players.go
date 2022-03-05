fb8cfd99fb0d43e0c296fd6c917bbd926ebda944
Julian Lawrance <julian.lawrance@gmail.com>
2022-03-05 13:00:44 +0000

    Move database logic for each resource type into separate files

---
+++ db/players.go
@@ -0,0 +1,61 @@
+package db
+
+import (
+	"context"
+	"log"
+	"phrasmotica/bore-score-api/models"
+	"time"
+
+	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func GetAllPlayers(ctx context.Context) ([]models.Player, bool) {
+	cursor, err := GetDatabase().Collection("Players").Find(ctx, bson.D{})
+	if err != nil {
+		log.Println(err)
+		return nil, false
+	}
+
+	var players []models.Player
+
+	err = cursor.All(ctx, &players)
+	if err != nil {
+		log.Println(err)
+		return nil, false
+	}
+
+	return players, true
+}
+
+func PlayerExists(ctx context.Context, username string) bool {
+	filter := bson.D{{"username", bson.D{{"$eq", username}}}}
+	result := GetDatabase().Collection("Players").FindOne(ctx, filter)
+	return result.Err() == nil
+}
+
+func AddPlayer(ctx context.Context, newPlayer *models.Player) bool {
+	newPlayer.ID = uuid.NewString()
+	newPlayer.TimeCreated = time.Now().UTC().Unix()
+
+	_, err := GetDatabase().Collection("Players").InsertOne(ctx, newPlayer)
+
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	return true
+}
+
+func DeletePlayer(ctx context.Context, username string) bool {
+	filter := bson.D{{"username", bson.D{{"$eq", username}}}}
+	_, err := GetDatabase().Collection("Players").DeleteOne(ctx, filter)
+
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	return true
+}
---
+++ db/results.go
@@ -0,0 +1,89 @@
+package db
+
+import (
+	"context"
+	"log"
+	"phrasmotica/bore-score-api/models"
+
+	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func GetAllResults(ctx context.Context) ([]models.Result, bool) {
+	cursor, err := GetDatabase().Collection("Results").Find(ctx, bson.D{})
+	if err != nil {
+		log.Println(err)
+		return nil, false
+	}
+
+	var results []models.Result
+
+	err = cursor.All(ctx, &results)
+	if err != nil {
+		log.Println(err)
+		return nil, false
+	}
+
+	return results, true
+}
+
+func AddResult(ctx context.Context, newResult *models.Result) bool {
+	newResult.ID = uuid.NewString()
+	_, err := GetDatabase().Collection("Results").InsertOne(ctx, newResult)
+
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	return true
+}
+
+func DeleteResultsWithGame(ctx context.Context, gameName string) (int64, bool) {
+	filter := bson.D{{"gameName", bson.D{{"$eq", gameName}}}}
+	deleteResult, err := GetDatabase().Collection("Results").DeleteMany(ctx, filter)
+
+	if err != nil {
+		log.Println(err)
+		return 0, false
+	}
+
+	return deleteResult.DeletedCount, true
+}
+
+func ScrubResultsWithPlayer(ctx context.Context, username string) (int64, bool) {
+	// filters to results where the given player took part
+	filter := bson.D{
+		{
+			"scores", bson.D{
+				{
+					"$elemMatch", bson.D{
+						{
+							"username", username,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// updates by setting the username field of the player's score object to an empty string
+	update := bson.D{
+		{
+			"$set", bson.D{
+				{
+					"scores.$.username", "",
+				},
+			},
+		},
+	}
+
+	result, err := GetDatabase().Collection("Results").UpdateMany(ctx, filter, update)
+
+	if err != nil {
+		log.Println(err)
+		return 0, false
+	}
+
+	return result.ModifiedCount, true
+}
