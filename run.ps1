11f9d0ce92dffd4a1862b93ca789183c3922e389
Julian L <julian.lawrance@gmail.com>
2023-04-15 10:44:52 +0100

    Improve script for running Docker container locally

--- run.ps1
+++ run.ps1
@@ -1,1 +1,21 @@
-docker run -e BORESCORE_ENV=production -d -p 8000:8000 phrasmotica/bore-score-api
+$name = "bore-score-api"
+$tag = "phrasmotica/bore-score-api"
+
+$runningName = $(docker ps --filter name=$name --format "{{.Names}}")
+
+if ($runningName) {
+    docker stop $name | Out-Null
+    Write-Host "Stopped running container $name"
+}
+
+$stoppedName = $(docker ps -a --filter name=$name --format "{{.Names}}")
+
+if ($stoppedName) {
+    docker rm $name | Out-Null
+    Write-Host "Removed stopped container $name"
+}
+
+$createdId = $(docker run --name $name -e BORESCORE_ENV=production -d -p 8000:8000 $tag)
+if ($createdId) {
+    Write-Host "Started container $name ($createdId)"
+}
