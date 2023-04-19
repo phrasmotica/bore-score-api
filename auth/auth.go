af6e9543f3225e422f57ff1914172adf9413378c
Julian L <julian.lawrance@gmail.com>
2023-04-19 22:39:06 +0100

    Add middleware for validating access token

--- auth/auth.go
+++ auth/auth.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt"
@@ -28,5 +29,33 @@ func GenerateJWT(email string, username string) (tokenString string, err error) 
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	tokenString, err = token.SignedString(jwtKey)
+	return
+}
+
+func ValidateToken(signedToken string) (err error) {
+	// TODO: return username/email claim so that we can pass it on to the request
+	token, err := jwt.ParseWithClaims(
+		signedToken,
+		&JWTClaim{},
+		func(token *jwt.Token) (interface{}, error) {
+			return []byte(jwtKey), nil
+		},
+	)
+
+	if err != nil {
+		return
+	}
+
+	claims, ok := token.Claims.(*JWTClaim)
+	if !ok {
+		err = errors.New("couldn't parse claims")
+		return
+	}
+
+	if claims.ExpiresAt < time.Now().Local().Unix() {
+		err = errors.New("token expired")
+		return
+	}
+
 	return
 }
