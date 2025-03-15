df320d11ad2179ac4533ddf33dc6497f490e46b4
Julian L <julian.lawrance@gmail.com>
2025-03-15 21:13:58 +0000

    Add more flexibility to debug.ps1, so that the service can be built and run independently of its dependencies

--- debug.ps1
+++ debug.ps1
@@ -1,16 +1,32 @@
 param(
     [switch] $Debugger = $false,
+    [switch] $BackendOnly = $false,
+    [switch] $DependenciesOnly = $false,
     [switch] $RunOnly = $false
 )
 
 $file = ".\docker-compose.yml"
+$serviceName = "backend"
 
 if ($Debugger) {
     $file = ".\docker-compose.debug.yml"
+    $serviceName = "backend-debug"
 }
 
 if (!$RunOnly) {
-    docker-compose -f .\docker-compose.dependencies.yml -f $file --env-file .\.env.docker build
+    if ($BackendOnly) {
+        docker-compose -f .\docker-compose.dependencies.yml -f $file --env-file .\.env.docker build $serviceName
+    }
+
+    if ($DependenciesOnly) {
+        docker-compose -f .\docker-compose.dependencies.yml .\.env.docker build
+    }
 }
 
-docker-compose -f .\docker-compose.dependencies.yml -f $file --env-file .\.env.docker up
+if ($BackendOnly) {
+    docker-compose -f .\docker-compose.dependencies.yml -f $file --env-file .\.env.docker up $serviceName
+}
+
+if ($DependenciesOnly) {
+    docker-compose -f .\docker-compose.dependencies.yml .\.env.docker up
+}
